17177c6b8822609291c955b2dd0a961dabc33362
sebasmmf <sebastian.mart.f@gmail.com>
2024-07-21 15:37:09 -0500

    Proyecto que calcula la propina segun el total de una orden utilizando TS y useReducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
+import { useReducer } from "react"
 import MenuItem from "./components/MenuItem"
 import OrderContents from "./components/OrderContents"
 import OrderTotals from "./components/OrderTotals"
 import TipPercentageForm from "./components/TipPercentageForm"
 import { menuItems } from "./data/db"
-import useOrder from "./hooks/useOrder"
+import { initialState, orderReducer } from "./reducers/order-reducer"
 
 function App() {
 
-  const { order, tip, setTip, addItem, removeItem, placeOrder } = useOrder()
+  const [state, dispatch] = useReducer(orderReducer, initialState)
 
   return (
     <>
@@ -24,27 +25,27 @@ function App() {
                 <MenuItem 
                   key={item.id}
                   item={item}
-                  addItem={addItem}
+                  dispatch={dispatch}
                 />
               ))}
             </div>
           </div>
 
           <div className="border border-dashed border-slate-300 p-5 rounded-lg space-y-10">
-            {order.length ? (
+            {state.order.length ? (
               <>
                   <OrderContents
-                    order={order}
-                    removeItem={removeItem}
+                    order={state.order}
+                    dispatch={dispatch}
                   />
                   <TipPercentageForm 
-                    setTip={setTip}
-                    tip={tip}
+                    dispatch={dispatch}
+                    tip={state.tip}
                   />
                   <OrderTotals 
-                    order={order}
-                    tip={tip}
-                    placeOrder={placeOrder}
+                    order={state.order}
+                    tip={state.tip}
+                    dispatch={dispatch}
                   />
               </>
             ) : (
--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -1,15 +1,17 @@
-import type {  MenuItem } from "../types";
+import { Dispatch } from "react";
+import { OrderActions } from "../reducers/order-reducer";
+import type { MenuItem } from "../types";
 
 type MenuItemProps = {
   item: MenuItem,
-  addItem: (item: MenuItem) => void
+  dispatch: Dispatch<OrderActions>
 }
 
-export default function MenuItem({item, addItem} : MenuItemProps) {
+export default function MenuItem({item, dispatch} : MenuItemProps) {
   return (
       <button 
         className=' border-2 border-teal-400 hover:bg-teal-200 p-3 text-lg  rounded-lg flex justify-between w-full'
-        onClick={() => addItem(item)}
+        onClick={() => dispatch({type:'add-item', payload:{item}})}
       > 
           <p>{item.name}</p> 
           <p className='font-black'>${item.price}</p>
